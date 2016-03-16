4a1d452570eea955e667f4ae59c362d7a71483e6
Seeker1437 <williams.darnell56@yahoo.com>
2016-03-16 11:14:53 -0400

    Removes Referenceless as System.Reactive is available.

--- src/Mabiavalon.Docking/Mabiavalon.Docking/DockItem.cs
+++ src/Mabiavalon.Docking/Mabiavalon.Docking/DockItem.cs
@@ -1,12 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Disposables;
 using System.Text;
 using System.Threading.Tasks;
+using Perspex.Controls;
 
 namespace Mabiavalon
 {
-    public class DockItem
+    public class DockItem : ContentControl
     {
+        public const string ThumbPartName = "PART_Thumb";
+
+        private readonly SerialDisposable _templateSubscriptions = new SerialDisposable();
+        private readonly SerialDisposable _rightMouseUpCleanUpDisposable = new SerialDisposable();
     }
 }
