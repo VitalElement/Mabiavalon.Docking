9e5bbf7b286c02308f045733dcdd11df6fdc0245
Seeker1437 <williams.darnell56@yahoo.com>
2016-03-16 11:09:20 -0400

    Implements Mabiavalon.Core
    * Dummy DockItem Class
    * Perspex Nuget Packages

---
+++ src/Mabiavalon.Docking/Mabiavalon.Docking/Core/FuncComparer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mabiavalon.Core
+{
+	internal class FuncComparer<TObject> : IComparer<TObject>
+	{
+		private readonly Func<TObject, TObject, int> _comparer;
+
+		public FuncComparer(Func<TObject, TObject, int> comparer)
+		{
+			if (comparer == null) throw new ArgumentNullException("comparer");
+
+			_comparer = comparer;
+		}
+
+		public int Compare(TObject x, TObject y)
+		{
+			return _comparer(x, y);
+		}
+	}
+}
---
+++ src/Mabiavalon.Docking/Mabiavalon.Docking/Core/TabHeaderDragStartInformation.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mabiavalon.Core
+{
+    internal class TabHeaderDragStartInformation
+    {
+        private readonly DockItem _dockItem;
+        private readonly double _dockItemsControlHorizontalOffset;
+        private readonly double _dockItemControlVerticalOffset;
+        private readonly double _dockItemHorizontalOffset;
+        private readonly double _dockItemVerticalOffset;
+
+        public TabHeaderDragStartInformation(
+            DockItem dockItem,
+            double dockItemsControlHorizontalOffset, double dockItemControlVerticalOffset, double dockItemHorizontalOffset, double dockItemVerticalOffset)
+        {
+            if (dockItem == null) throw new ArgumentNullException("dragItem");
+
+            _dockItem = dockItem;
+            _dockItemsControlHorizontalOffset = dockItemsControlHorizontalOffset;
+            _dockItemControlVerticalOffset = dockItemControlVerticalOffset;
+            _dockItemHorizontalOffset = dockItemHorizontalOffset;
+            _dockItemVerticalOffset = dockItemVerticalOffset;
+        }
+
+        public DockItem Item
+        {
+            get { return _dockItem; }
+        }
+
+        public double DockItemsControlHorizontalOffset
+        {
+            get { return _dockItemsControlHorizontalOffset; }
+        }
+
+        public double DockItemControlVerticalOffset
+        {
+            get { return _dockItemControlVerticalOffset; }
+        }
+
+        public double DockItemHorizontalOffset
+        {
+            get { return _dockItemHorizontalOffset; }
+        }
+
+        public double DockItemVerticalOffset
+        {
+            get { return _dockItemVerticalOffset; }
+        }
+    }
+}
