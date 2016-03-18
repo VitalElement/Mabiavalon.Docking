73634e0eb71c8ee4de81c7f21924e4975c430782
Seeker1437 <williams.darnell56@yahoo.com>
2016-03-17 22:09:12 -0400

    Add ItemActionCallbackArgs

---
+++ src/Mabiavalon.Docking/ItemActionCallbackArgs.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Perspex.Controls;
+
+namespace Mabiavalon
+{
+    public delegate void ItemActionCallback(ItemActionCallbackArgs<DockControl> args);
+
+    public class ItemActionCallbackArgs<TOwner> where TOwner : Control
+    {
+        private readonly Window _window;
+        private readonly TOwner _owner;
+        private readonly DockItem _dockItem;
+
+        public ItemActionCallbackArgs(Window window, TOwner owner, DockItem dockItem)
+        {
+            if (window == null) throw new ArgumentNullException("window");
+            if (owner == null) throw new ArgumentNullException("owner");
+            if (dockItem == null) throw new ArgumentNullException("dockItem");
+
+            _window = window;
+            _owner = owner;
+            _dockItem = dockItem;
+        }
+
+        public Window Window
+        {
+            get { return _window; }
+        }
+
+        public TOwner Owner
+        {
+            get { return _owner; }
+        }
+
+        public DockItem DockItem
+        {
+            get { return _dockItem; }
+        }
+
+        public bool IsCancelled { get; private set; }
+
+        public void Cancel()
+        {
+            IsCancelled = true;
+        }
+    }
+}
