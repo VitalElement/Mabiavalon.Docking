80a9de8bca366d1c97c776b85ff552690b27d729
Seeker1437 <williams.darnell56@yahoo.com>
2016-03-17 21:34:42 -0400

    Adds BranchResult

---
+++ src/Mabiavalon.Docking/Docking/BranchResult.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mabiavalon.Docking
+{
+    public class BranchResult
+    {
+        private readonly Branch _branch;
+        private readonly DockControl _dockControl;
+
+        public BranchResult(Branch branch, DockControl dockControl)
+        {
+            if (branch == null) throw new ArgumentNullException("branch");
+            if (dockControl == null) throw new ArgumentNullException("dockControl");
+
+            _branch = branch;
+            _dockControl = dockControl;
+        }
+
+        public Branch Branch
+        {
+            get { return _branch; }
+        }
+
+        public DockControl DockControl
+        {
+            get { return _dockControl; }
+        }
+    }
+}
