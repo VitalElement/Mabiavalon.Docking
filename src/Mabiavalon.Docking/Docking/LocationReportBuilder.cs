c5d47b29c01c30cfcb06a8dc672b4565749b4e51
Seeker1437 <williams.darnell56@yahoo.com>
2016-03-17 21:55:29 -0400

    Add LocationReportBuilder

---
+++ src/Mabiavalon.Docking/Docking/LocationReportBuilder.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mabiavalon.Docking
+{
+    public class LocationReportBuilder
+    {
+        private readonly DockControl _targetDockControl;
+        private Branch _branch;
+        private bool _isSecondLeaf;
+        private Layout _layout;
+
+        public LocationReportBuilder(DockControl targetDockControl)
+        {
+            _targetDockControl = targetDockControl;
+        }
+
+        public DockControl TargetDockControl
+        {
+            get { return _targetDockControl; }
+        }
+
+        public bool IsFound { get; private set; }
+
+        public void MarkFound()
+        {
+            if (IsFound)
+                throw new InvalidOperationException("Already found.");
+
+            IsFound = true;
+
+            _layout = CurrentLayout;
+        }
+
+        public void MarkFound(Branch branch, bool isSecondLeaf)
+        {
+            if (branch == null) throw new ArgumentNullException("branch");
+            if (IsFound)
+                throw new InvalidOperationException("Already found.");
+
+            IsFound = true;
+
+            _layout = CurrentLayout;
+            _branch = branch;
+            _isSecondLeaf = isSecondLeaf;
+        }
+
+        public Layout CurrentLayout { get; set; }
+
+        public LocationReport ToLocationReport()
+        {
+            return new LocationReport(_targetDockControl, _layout, _branch, _isSecondLeaf);
+        }
+    }
+}
