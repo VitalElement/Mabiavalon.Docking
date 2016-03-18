13aa03870a19ac720e200f6a18895223367a5c9c
Seeker1437 <williams.darnell56@yahoo.com>
2016-03-17 20:28:57 -0400

    Adds Horizontal and Vertical Organizers

---
+++ src/Mabiavalon.Docking/VerticalOrganizer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Perspex.Controls;
+
+namespace Mabiavalon
+{
+    public class VerticalOrganizer : StackOrganizer
+    {
+        public VerticalOrganizer() : base(Orientation.Vertical)
+        {
+            
+        }
+    }
+}
