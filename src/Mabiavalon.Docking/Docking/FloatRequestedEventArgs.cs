237264b1bcfe2a569eba6a6ecb85ab9bad9ec564
Seeker1437 <williams.darnell56@yahoo.com>
2016-03-18 01:38:41 -0400

    Add FloatRequestedEventArgs

---
+++ src/Mabiavalon.Docking/Docking/FloatRequestedEventArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Perspex.Interactivity;
+
+namespace Mabiavalon.Docking
+{
+    public delegate void FloatRequestedEventHandler(object sender, FloatRequestedEventArgs e);
+
+    public class FloatRequestedEventArgs : DockItemEventArgs
+    {
+        private readonly DockItem _dockItem;
+
+        public FloatRequestedEventArgs(RoutedEvent routedEvent, IInteractive source, DockItem dockItem)
+            : base(routedEvent, source, dockItem)
+        {
+            _dockItem = dockItem;
+        }
+
+        public FloatRequestedEventArgs(RoutedEvent routedEvent, DockItem dockItem)
+            : base(routedEvent, dockItem)
+        { }
+    }
+}
