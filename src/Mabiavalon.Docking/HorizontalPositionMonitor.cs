c5f5d2621b6b3a27ab327964272597ece25e759f
Seeker1437 <williams.darnell56@yahoo.com>
2016-03-17 21:06:26 -0400

    Add HorizontalPositionMonitor

---
+++ src/Mabiavalon.Docking/HorizontalPositionMonitor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Perspex.Controls;
+
+namespace Mabiavalon
+{
+    public class HorizontalPositionMonitor : StackPositionMonitor
+    {
+        public HorizontalPositionMonitor() : base(Orientation.Horizontal)
+        {
+        }
+    }
+}
