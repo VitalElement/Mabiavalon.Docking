b8d6a5936efda74591264a1c732482c29e4a70c1
Seeker1437 <williams.darnell56@yahoo.com>
2016-03-18 01:39:16 -0400

    Add FloatTransfer

---
+++ src/Mabiavalon.Docking/Docking/FloatTransfer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mabiavalon.Docking
+{
+    internal class FloatTransfer
+    {
+        private readonly double _width;
+        private readonly double _height;
+        private readonly object _content;
+
+        public FloatTransfer(double width, double height, object content)
+        {
+            if (content == null) throw new ArgumentNullException("content");
+
+            _width = width;
+            _height = height;
+            _content = content;
+        }
+
+        public static FloatTransfer TakeSnapshot(DockItem dockItem, DockControl sourceDockControl)
+        {
+            if (dockItem == null) throw new ArgumentNullException("dockItem");
+
+            return new FloatTransfer(sourceDockControl.Width, sourceDockControl.Height, dockItem.UnderlyingContent ?? dockItem.Content ?? dockItem);
+        }
+
+        [Obsolete]
+        //TODO width and height transfer obsolete
+        public double Width
+        {
+            get { return _width; }
+        }
+
+        [Obsolete]
+        //TODO width and height transfer obsolete
+        public double Height
+        {
+            get { return _height; }
+        }
+
+        public object Content
+        {
+            get { return _content; }
+        }
+    }
+}
