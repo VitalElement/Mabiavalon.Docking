d58967591b4a801696fd42609247eda8f593fc55
Seeker1437 <williams.darnell56@yahoo.com>
2016-03-17 21:39:16 -0400

    Adds DropZone

---
+++ src/Mabiavalon.Docking/Docking/DropZone.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Perspex;
+using Perspex.Controls.Primitives;
+
+namespace Mabiavalon.Docking
+{
+    public class DropZone : TemplatedControl
+    {
+        private DropZoneLocation _location;
+        private bool _isOffered;
+
+        public static readonly DirectProperty<DropZone, DropZoneLocation> LocationProperty =
+            PerspexProperty.RegisterDirect<DropZone, DropZoneLocation>("Location", o => o.Location,
+                (o, v) => o.Location = v);
+
+        public static readonly DirectProperty<DropZone, bool> IsOfferedProperty =
+            PerspexProperty.RegisterDirect<DropZone, bool>("IsOffered", o => o.IsOffered);
+
+        static DropZone()
+        {
+            // Style
+        }
+
+        public DropZoneLocation Location
+        {
+            get { return _location; }
+            set { SetAndRaise(LocationProperty, ref _location, value); }
+        }
+
+        public bool IsOffered
+        {
+            get { return _isOffered; }
+            internal set { SetAndRaise(IsOfferedProperty, ref _isOffered, value); }
+        }
+    }
+}
