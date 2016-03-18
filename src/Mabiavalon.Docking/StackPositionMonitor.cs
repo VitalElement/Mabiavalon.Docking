8c655fdbadff614516983b68cd90d930839aece4
Seeker1437 <williams.darnell56@yahoo.com>
2016-03-17 21:04:48 -0400

    Add StackPositionMonitor + OrderChangedEventArgs

---
+++ src/Mabiavalon.Docking/StackPositionMonitor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Perspex.Controls;
+
+namespace Mabiavalon
+{
+    /// <summary>
+    /// A linear position monitor simplifies the montoring of the order of items, where they are laid out
+    /// horizontally or vertically (typically via a <see cref="StackOrganizer"/>.
+    /// </summary>
+    public abstract class StackPositionMonitor : PositionMonitor
+    {
+        private readonly Func<DockItem, double> _getLocation;
+
+        protected StackPositionMonitor(Orientation orientation)
+        {
+            switch (orientation)
+            {
+                case Orientation.Horizontal:
+                    _getLocation = item => item.X;
+                    break;
+                case Orientation.Vertical:
+                    _getLocation = item => item.Y;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("orientation");
+            }
+        }
+
+        public event EventHandler<OrderChangedEventArgs> OrderChanged;
+
+        internal virtual void OnOrderChanged(OrderChangedEventArgs e)
+        {
+            var handler = OrderChanged;
+            if (handler != null) handler(this, e);
+        }
+
+        internal IEnumerable<DockItem> Sort(IEnumerable<DockItem> items)
+        {
+            if (items == null) throw new ArgumentNullException("items");
+
+            return items.OrderBy(i => _getLocation(i));
+        }
+    }
+}
