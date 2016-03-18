14a6f9ebd6ac84c48c8d32d10fa961d495727ad1
Seeker1437 <williams.darnell56@yahoo.com>
2016-03-17 21:35:31 -0400

    Adds DropZoneLocation

---
+++ src/Mabiavalon.Docking/Docking/DropZoneLocation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mabiavalon.Docking
+{
+    public enum DropZoneLocation
+    {
+        Top,
+        Right,
+        Bottom,
+        Left,
+        Floating
+    }
+}
