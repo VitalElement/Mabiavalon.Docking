6696afd5cd73735c5efc20eabd5c987a0bfdcb8d
Seeker1437 <williams.darnell56@yahoo.com>
2016-03-17 22:05:06 -0400

    Add Tiler

---
+++ src/Mabiavalon.Docking/Docking/Tiler.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Perspex;
+using Perspex.Controls;
+using Perspex.Layout;
+
+namespace Mabiavalon.Docking
+{
+    internal class Tiler
+    {
+        public static void Tile(IEnumerable<DockItem> dockItems, Size bounds)
+        {
+            if (dockItems == null) throw new ArgumentNullException("dockItems");
+
+            var items = new Queue<DockItem>(dockItems.OrderBy(di => di.GetValue(Visual.ZIndexProperty)));
+
+            var cellCountPerColumn = TilerCalculator.GetCellCountPerColumn(items.Count());
+            var x = 0d;
+            var cellWidth = bounds.Width / cellCountPerColumn.Length;
+            foreach (var cellCount in cellCountPerColumn)
+            {
+                var y = 0d;
+                var cellHeight = bounds.Height / cellCount;
+                for (var cell = 0; cell < cellCount; cell++)
+                {
+                    var item = items.Dequeue();
+                    Layout.SetFloatingItemState(item, WindowState.Normal);
+                    item.SetValue(DockItem.XProperty, x);
+                    item.SetValue(DockItem.YProperty, y);
+                    item.SetValue(Layoutable.WidthProperty, cellWidth);
+                    item.SetValue(Layoutable.HeightProperty, cellHeight);
+
+                    y += cellHeight;
+                }
+
+                x += cellWidth;
+            }
+        }
+
+        public static void TileHorizontally(IEnumerable<DockItem> dockItems, Size bounds)
+        {
+            if (dockItems == null) throw new ArgumentNullException("dockItems");
+
+            var items = dockItems.ToList();
+
+            var x = 0.0;
+            var width = bounds.Width / items.Count;
+            foreach (var dragablzItem in items)
+            {
+                Layout.SetFloatingItemState(dragablzItem, WindowState.Normal);
+                dragablzItem.SetValue(DockItem.XProperty, x);
+                dragablzItem.SetValue(DockItem.YProperty, 0d);
+                x += width;
+                dragablzItem.SetValue(Layoutable.WidthProperty, width);
+                dragablzItem.SetValue(Layoutable.HeightProperty, bounds.Height);
+            }
+        }
+
+        public static void TileVertically(IEnumerable<DockItem> dockItems, Size bounds)
+        {
+            if (dockItems == null) throw new ArgumentNullException("dockItems");
+
+            var items = dockItems.ToList();
+
+            var y = 0.0;
+            var height = bounds.Height / items.Count;
+            foreach (var dragablzItem in items)
+            {
+                Layout.SetFloatingItemState(dragablzItem, WindowState.Normal);
+                dragablzItem.SetValue(DockItem.YProperty, y);
+                dragablzItem.SetValue(DockItem.XProperty, 0d);
+                y += height;
+                dragablzItem.SetValue(Layoutable.HeightProperty, height);
+                dragablzItem.SetValue(Layoutable.WidthProperty, bounds.Width);
+            }
+        }
+
+    }
+}
